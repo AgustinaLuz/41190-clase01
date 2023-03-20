feb95da77e54e5eab0763eb45f490e39cc2d0ba4
AgustinaLuz <agusluzcrdz@gmail.com>
2023-03-20 19:16:23 -0300

    testing new branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first project: clase02';
+  title = 'first project: test branch';
 }
