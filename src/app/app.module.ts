6585f1a1074e3805a100c0befc766a9ff374e893
AgustinaLuz <agusluzcrdz@gmail.com>
2023-03-20 11:30:47 -0300

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
+import { PublicityComponent } from './publicity/publicity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    SidebarComponent,
+    HomeComponent,
+    PageWrapperComponent,
+    PublicityComponent
   ],
   imports: [
     BrowserModule
