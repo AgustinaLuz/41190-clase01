1aeae6ae4b52d6d69a22e49ee26d148c1784efc4
AgustinaLuz <agusluzcrdz@gmail.com>
2023-03-20 18:52:43 -0300

    finished creating components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'first project: clase01';
+  title = 'first project: clase02';
 }
